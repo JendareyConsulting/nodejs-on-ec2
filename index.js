f956bdf0b34de94b3d0e23208de6d5b7513985f2
akinluyigithub <134658948+akinluyigithub@users.noreply.github.com>
2023-07-21 22:47:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome Jendarey Consulting, Inc. Working to improve you! Need a career change and a pay increase? We invite you to an online Anti-Money Laundering Career Training lanre.'); //write a response to the client
+  res.write('Welcome Jendarey Consulting, Inc. Working to improve you! Need a career change and a pay increase? We invite you to an online Anti-Money Laundering Career Training lanre. ..'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
